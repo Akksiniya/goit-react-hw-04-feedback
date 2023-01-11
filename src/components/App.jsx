b7c968822b96e60869e7efe8d4e9df1918c50435
akksinya <akksin.ya999@gmail.com>
2023-01-11 10:52:49 +0200

    edit options app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,7 +38,8 @@ export class App extends Component {
       <>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
+            // options={['good', 'neutral', 'bad']}
+            options={Object.keys(this.state)}
             onLeaveFeedback={this.handleIncrement}
           />
         </Section>
